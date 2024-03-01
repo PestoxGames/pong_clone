ccbda092223907a51d367366672bc163252d8ecd
Jose Pedro Silva <josepedro88@live.com.pt>
2024-03-01 19:29:48 +0100

    Update paddle AI speed and animation

--- Scenes/paddle_ai/paddle_ai.gd
+++ Scenes/paddle_ai/paddle_ai.gd
@@ -1,30 +1,44 @@
 extends CharacterBody2D
 
 @onready var ball = $"../Ball"
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
-@export var _speed = 1000
-@export var ball_tracking_acc = 0.8
+@export var _speed = 800
+@export var ball_tracking_acc = 0.85
+
+var _speed_compensation: float = 60.0
 
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	animated_sprite_2d.play("idle")
 	
 func _physics_process(delta):
 	var _clamp_max = Vector2(screen_size.x, screen_size.y - 50) 
 	var ball_position = ball.position
-	var target_y = ball_position.y * ball_tracking_acc + position.y * (1 - ball_tracking_acc)
-
-	var velocity = Vector2.ZERO
+	var target_y = ball_position.y * ball_tracking_acc
+	
+	var difference = target_y - position.y
+	var error_adjustment = 0.1
+	
+	var _direction: Vector2 = Vector2.ZERO
 	
 	if target_y > position.y:
-		velocity.y += 1
+		_direction.y += 1
 	
 	elif target_y < position.y:
-		velocity.y -= 1
+		_direction.y -= 1
 	
-	velocity = velocity * _speed
+	if velocity.y > 0:
+		animated_sprite_2d.play("move_down")
+	if velocity.y < 0:
+		animated_sprite_2d.play("move_up")
+	if velocity.y == 0:
+		animated_sprite_2d.play("idle")
+		
+	_direction.y = clamp(difference * error_adjustment, -1, 1)
 	
-	position += velocity * delta
-	position = position.clamp(Vector2(0, 50), _clamp_max)
-
+	velocity = _direction * (_speed * _speed_compensation) * delta 
+	
+	move_and_slide()
